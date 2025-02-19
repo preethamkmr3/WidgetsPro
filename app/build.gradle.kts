768c3d6df7f10825f01e521519e642a66663fabc
Preetham <preethamthallam@gmail.com>
2025-02-20 01:32:05 +0530

    Few improvements and added battery widget

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 34
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.2"
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
