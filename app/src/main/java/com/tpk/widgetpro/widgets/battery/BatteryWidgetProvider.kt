013566a0e58515340450bb7197ed771b39bd6df1
preethamkmr3 <preethamthallam@gmail.com>
2025-03-02 18:09:05 +0530

    Fix battery widget crash on adding from launcher's widget section

--- app/src/main/java/com/tpk/widgetpro/widgets/battery/BatteryWidgetProvider.kt
+++ app/src/main/java/com/tpk/widgetpro/widgets/battery/BatteryWidgetProvider.kt
@@ -1,17 +1,12 @@
 package com.tpk.widgetpro.widgets.battery
 
 import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
-import com.tpk.widgetpro.MainActivity
-import com.tpk.widgetpro.R
-import com.tpk.widgetpro.base.BaseWidgetProvider
 import com.tpk.widgetpro.services.BatteryMonitorService
 
-class BatteryWidgetProvider : BaseWidgetProvider() {
-    override val layoutId = R.layout.battery_widget_layout
-    override val setupText = "Tap to setup Battery"
-    override val setupDestination = MainActivity::class.java
+class BatteryWidgetProvider : AppWidgetProvider() {
 
     override fun onEnabled(context: Context) {
         super.onEnabled(context)
@@ -23,7 +18,11 @@ class BatteryWidgetProvider : BaseWidgetProvider() {
         context.stopService(Intent(context, BatteryMonitorService::class.java))
     }
 
-    override fun updateNormalWidgetView(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-        context.startService(Intent(context, BatteryMonitorService::class.java))
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
     }
 }
